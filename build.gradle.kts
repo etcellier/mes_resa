d324ffc55fd4b46419b1b0a0750c6fcddccac51b
Etienne Cellier <etienne.cellier@next-u.fr>
2024-10-07 20:36:52 +0200

    MAJ config gradle prise en compte du driver.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation(files("libs/ojdbc11.jar"))
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
